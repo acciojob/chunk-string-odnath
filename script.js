1569ad0025e0a688b68e7cc73ccd6bcc93e850ca
odnath <od.nathmnair1@gmail.com>
2025-05-13 13:30:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-function stringChop(str, size) {
-  // your code here
-}
+function chunkString(str, chunkLength) {
+  if (str === null || typeof str !== 'string') return [];
 
-// Do not change the code below
-const str = prompt("Enter String.");
-const size = prompt("Enter Chunk Size.");
-alert(stringChop(str, size));
+  const result = [];
+  for (let i = 0; i < str.length; i += chunkLength) {
+    result.push(str.slice(i, i + chunkLength));
+  }
+  return result;
+}
